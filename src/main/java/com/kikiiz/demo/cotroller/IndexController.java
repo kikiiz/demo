d412e57abd36d3ca786bf0cdae19ccc9b999a9cc
kikiiz <chenqskin@126.com>
2019-03-01 02:44:12 +0800

    Freemaker遍历List

--- src/main/java/com/kikiiz/demo/cotroller/IndexController.java
+++ src/main/java/com/kikiiz/demo/cotroller/IndexController.java
@@ -4,6 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @Controller
 public class IndexController {
     @RequestMapping(path={"/","/index"},method = {RequestMethod.GET})
@@ -24,6 +29,13 @@ public class IndexController {
     @RequestMapping(path={"/vm"},method = {RequestMethod.GET})
     public String template(Model model){
         model.addAttribute("value1","abc");
+        List<String> colors = Arrays.asList(new String []{"red","green","blue"});
+        model.addAttribute("colors",colors);
+        Map<String,String> map=new HashMap<>();
+        for(int i=0;i<4;i++){
+            map.put(String.valueOf(i),String.valueOf(i*i));
+        }
+        model.addAttribute("map",map);
         return "home";
     }
 }
